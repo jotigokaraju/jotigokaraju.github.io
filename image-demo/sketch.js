41ccd242150a516596befdf173ab1c36d1557580
jotigokaraju <jotigokaraju1234567890@gmail.com>
2024-09-24 10:00:24 -0600

    commit new changes

---
+++ image-demo/sketch.js
@@ -0,0 +1,26 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+
+let spongebob;
+
+function preload() {
+  spongebob = loadImage("Spongebob.png");
+
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+
+  image(spongebob, mouseX, mouseY);
+}
