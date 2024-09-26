aa4386e2e34e70fef40281eb9a3774690889d8d5
jotigokaraju <jotigokaraju1234567890@gmail.com>
2024-09-26 10:25:28 -0600

    add drawcircle function

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -9,28 +9,42 @@
 let handPose;
 let video;
 let hands = [];
+let circleX = 340;
+let circleY = 220;
+const circleDiameter = 50;
+const keysWASD = [87, 65, 83, 68];
+let moveAmount = 5;
 
 function preload() {
   // Load the handPose model
   handPose = ml5.handPose();
 }
 
+
 function setup() {
-  createCanvas(640, 480);
-  // Create the webcam video and hide it
+  //createCanvas(640, 480);
+  createCanvas(windowWidth, windowHeight);
+  
   video = createCapture(VIDEO);
-  video.size(640, 480);
+  //video.size(640, 480);
+  video.size(windowWidth, windowHeight);
   video.hide();
-  // start detecting hands from the webcam video
+  
   handPose.detectStart(video, gotHands);
 }
 
 function draw() {
-  // Draw the webcam video
+  
+  
+
   translate(video.width, 0);
   scale(-1, 1);
   image(video, 0, 0, width, height);
-  // Draw all the tracked hand points
+
+  
+  drawcircle();
+
+  
   for (let i = 0; i < hands.length; i++) {
     let hand = hands[i];
     for (let j = 0; j < hand.keypoints.length; j++) {
@@ -38,12 +52,38 @@ function draw() {
       fill(0, 255, 0);
       noStroke();
       circle(keypoint.x, keypoint.y, 10);
+      
     }
   }
+
+  
+
 }
 
 // Callback function for when handPose outputs data
 function gotHands(results) {
   // save the output to the hands variable
   hands = results;
+}
+
+
+function drawcircle() {
+  fill("limegreen");
+  circle(circleX, circleY, circleDiameter);
+
+  if (keyIsDown(keysWASD[0]) === true) {
+    circleY -= moveAmount;
+  }
+
+  if (keyIsDown(keysWASD[1]) === true) {
+    circleX += moveAmount;
+}
+
+  if (keyIsDown(keysWASD[2]) === true) {
+    circleY += moveAmount;
+  }
+
+  if (keyIsDown(keysWASD[3]) === true) {
+    circleX -= moveAmount;
+  }
 }
