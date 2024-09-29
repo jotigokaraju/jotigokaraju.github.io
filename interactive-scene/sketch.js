04007065b67ba7503b7dcaf31d82db06acab32fc
jotigokaraju <jotigokaraju1234567890@gmail.com>
2024-09-28 19:28:08 -0600

    add in functionality

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,118 +1,17 @@
-/*
+
 
 let circleX = 340;
 let circleY = 220;
 const circleDiameter = 50;
 const keysWASD = [87, 65, 83, 68];
 let moveAmount = 50;
-
-//variable for handpose ml model
-let handpose;
-//variable to store the video feed
-let video;
-//array for hand point predictions
-let predictions = [];
-
-function setup() {
-  
- createCanvas(640, 480);
-
- video = createCapture(VIDEO);
- video.size(width, height);
-
-
-  print("loading")
-  // call modelReady() when it is loaded
-  handpose = ml5.handpose(video, modelReady);
-  // Hide the video element, and just show the canvas
-  video.hide();
-}
-// when the model is ready, a message appears in the console and it predicts where each landmark should be placed
-function modelReady() {
-  console.log("Model ready!");
-    handpose.on("predict", function(results) {
-    predictions = results;
-  });
-  handpose.predict(video);
-}
-
-function draw() {
-  // render the video feed
-  image(video, 0, 0, width, height);
-  // We can call a function to draw using the keypoints
-  drawObject();
-}
-// A function to draw a ball at the tip of the finger
-function drawObject() {
-  if (predictions.length > 0) {
-    let prediction = predictions[0];
-    let x = prediction.annotations.indexFinger[3][0]
-    let y = prediction.annotations.indexFinger[3][1]
-    print(prediction, x, y)
-    fill(51);
-    noStroke();
-  
-    // A small ellipse to track the finger
-    ellipse(round(x), round(y), 50, 50) 
-      
-    // If the finger point is in the left of the screen, draw the first rectangle; otherwise draw the second
-    if (x < (300)) {
-      rect(0, 0, 300, 480);  // Left
-    }
-    else {
-      rect(300, 0, 300, 480); // Right
-    }
-  }
-}
-
-
-
-function drawcircle() {
-  fill("limegreen");
-  circle(circleX, circleY, circleDiameter);
-
-  
-  if (keyIsDown(keysWASD[0]) === true) {
-    if (circleY > circleDiameter/2) {
-      circleY -= moveAmount;
-    }
-  }
-
-  if (keyIsDown(keysWASD[1]) === true) {
-    if (circleX < windowWidth - circleDiameter/2) {
-      circleX += moveAmount;
-    }
-  }
-
-  if (keyIsDown(keysWASD[2]) === true) {
-    if (circleY < windowHeight - circleDiameter/2) {
-      circleY += moveAmount;
-    }
-    
-  }
-
-  if (keyIsDown(keysWASD[3]) === true) {
-    if (circleX > circleDiameter/2) {
-      circleX -= moveAmount;
-    }
-    
-  }
-}
-
-*/
-
-
-/*
- * 👋 Hello! This is an ml5.js example made and shared with ❤️.
- * Learn more about the ml5.js project: https://ml5js.org/
- * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
- *
- * This example demonstrates tracking particular parts of the hand through ml5.handPose.
- */
-
 let handPose;
 let video;
 let hands = [];
+let vidAspectRatio = video.width / video.height;
+let canAspectRatio = windowWidth / windowHeight;
+let VideoWidth;
+let VideoHeight;
 
 // A variable to track a pinch between thumb and index
 let pinch = 0;
@@ -124,9 +23,11 @@ function preload() {
 
 function setup() {
   createCanvas(640, 480);
-  // Create the webcam video and hide it
   video = createCapture(VIDEO);
+  
+
   video.size(640, 480);
+  
   video.hide();
   // Start detecting hands from the webcam video
   handPose.detectStart(video, gotHands);
@@ -134,22 +35,38 @@ function setup() {
 
 function draw() {
   // Draw the webcam video
-  image(video, 0, 0, width, height);
+
+  
+  translate(video.width, 0);
+  //then scale it by -1 in the x-axis
+  //to flip the image
+  scale(-1, 1);
+  //draw video capture feed as image inside p5 canvas
+  image(video, 0, 0);
+  
+
 
   // If there is at least one hand
   for (let i = 0; i < hands.length; i++) {
-    // Find the index finger tip and thumb tip
-    let finger = hands[0].index_finger_tip;
-    console.log(i);
-    
-    
 
+    if (i === 0) {
+    
+      let finger = hands[0].index_finger_tip;
+      console.log(i);
+      fill(0, 255, 0, 200);
+      circle(finger.x, finger.y, 50);
 
-    // This circle's size is controlled by a "pinch" gesture
-    fill(0, 255, 0, 200);
-    stroke(0);
-    strokeWeight(2);
-    circle(finger.x, finger.y, 50);
+    }
+
+    else {
+      let hand = hands[i]
+      for (let j = 0; j < hand.keypoints.length; j++) {
+        let keypoint = hand.keypoints[j];
+        fill(0, 255, 0);
+        noStroke();
+        circle(keypoint.x, keypoint.y, 10);
+      }
+    }
 
   }
 }
