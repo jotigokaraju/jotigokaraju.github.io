b7182574e1303551db958844d3c16f2795e03845
jotigokaraju <jotigokaraju1234567890@gmail.com>
2024-11-14 10:27:55 -0600

    add all of last night's and today's work from laptop
    Error - Cannon balls lag behind as space clicks get slower & slower for some reason.

--- 2d-array-assignment/sketch.js
+++ 2d-array-assignment/sketch.js
@@ -1,9 +1,3 @@
-// Grid Demo
-// Dan Schellenberg
-// Oct 22, 2024
-
-
-
 let grid;
 let cellWidth;
 const GRID_SIZE = 35;
@@ -24,7 +18,9 @@ let cannonHeight;
 let cannon;
 let rotationAngle;
 let fireCannon;
-let ballFired;
+let ballFired = [];
+let bubbleFired = false;
+let count_of_balls = -1;
 
 function preload() {
   blaster = loadImage('blaster.png');
@@ -33,23 +29,21 @@ function preload() {
   greenBubble = loadImage('greenbubble.png');
   blueBubble = loadImage('bluebubble.png');
   cannon = loadImage('cannon.png');
-  
 }
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cellHeight = Math.floor(windowHeight/GRID_SIZE/1.1/2.5);
-  cellWidth = Math.floor((windowWidth-40)/((1+SPACING_BETWEEN_BUBBLES)*35));
+  cellHeight = Math.floor(windowHeight / GRID_SIZE / 1.1 / 2.5);
+  cellWidth = Math.floor((windowWidth - 40) / ((1 + SPACING_BETWEEN_BUBBLES) * 35));
   colours = [purpleBubble, redBubble, greenBubble, blueBubble];
   grid = generateRandomGrid(cellHeight, cellWidth);
   angleMode(DEGREES);  
-  cannonHeight = 0.3*windowHeight;
-  cannonY = 0.8*windowHeight; 
-  cannonWidth = 0.2*windowWidth; 
-  cannonX = windowWidth/2 - 0.5*cannonWidth; 
+  cannonHeight = 0.3 * windowHeight;
+  cannonY = 0.8 * windowHeight; 
+  cannonWidth = 0.2 * windowWidth; 
+  cannonX = windowWidth / 2 - 0.5 * cannonWidth; 
   fireCannon = new Cannon(cannonHeight, cannonWidth, cannonX, cannonY);
-  ballFired = new FiringBall();
+  
 }
 
 function draw() {
@@ -57,12 +51,26 @@ function draw() {
   displayGrid();
   fireCannon.move();
   fireCannon.show(cannon);
-  console.log(windowWidth);
-  ballFired.fire("blue");
+
+  for (let ballNumber = 0; ballNumber < count_of_balls; ballNumber++) {
+    console.log("Intialized");
+    ballFired[ballNumber].move();
+    ballFired[ballNumber].display();
+    ballFired[ballNumber].checkCollision();
+  }
+  
 }
 
+function keyPressed() {
+  if (key === ' ') {
+    bubbleFired = true;
+    console.log(bubbleFired);
 
-
+    ballFired.push(new FiringBall());
+    count_of_balls += 1;
+    ballFired[count_of_balls].initialize(fireCannon.x + cannonWidth/4, fireCannon.y, random(colours));
+  }
+}
 
 function displayGrid() {
   for (let y = 0; y < cellHeight; y++) {
@@ -74,7 +82,6 @@ function displayGrid() {
   }
 }
 
-
 function generateRandomGrid(cols, rows) {
   let xStart;
   let newGrid = [];
@@ -82,15 +89,15 @@ function generateRandomGrid(cols, rows) {
   for (let y = 0; y < cols; y++) {
     newGrid.push([]);
     for (let x = 0; x < rows; x++) {
-      yStart = (y+1)*1.1*GRID_SIZE;
-      if (y%2 === 0) {
-        xStart = GRID_SIZE*(1+SPACING_BETWEEN_BUBBLES)*(x)+INDENT_BUBBLES;
+      yStart = (y + 1) * 1.1 * GRID_SIZE;
+      if (y % 2 === 0) {
+        xStart = GRID_SIZE * (1 + SPACING_BETWEEN_BUBBLES) * x + INDENT_BUBBLES;
+      } else {
+        xStart = GRID_SIZE * (1 + SPACING_BETWEEN_BUBBLES) * x + 2 * INDENT_BUBBLES;
       }
-      else {
-        xStart = GRID_SIZE*(1+SPACING_BETWEEN_BUBBLES)*(x)+2*INDENT_BUBBLES;
-      }
-      newGrid[y].push({active:true, 
-        colour:colours[Math.floor(Math.random() * colours.length)],
+      newGrid[y].push({
+        active: true, 
+        colour: colours[Math.floor(Math.random() * colours.length)],
         xPosition: xStart,
         yPosition: yStart,
       });
@@ -98,7 +105,6 @@ function generateRandomGrid(cols, rows) {
   }
   return newGrid;  
 }
-
 
 class Cannon {
   constructor(cannonHeight, cannonWidth, cannonX, cannonY) {
@@ -110,11 +116,11 @@ class Cannon {
   }
 
   move() {
-    if (this.x > windowWidth-cannonWidth/2) {
-      this.direction = this.direction*-1;
+    if (this.x > windowWidth - cannonWidth / 2) {
+      this.direction = this.direction * -1;
     }
-    if (this.x < 0-cannonWidth/4) {
-      this.direction = this.direction*-1;
+    if (this.x < 0 - cannonWidth / 4) {
+      this.direction = this.direction * -1;
     }
 
     this.x += this.direction;
@@ -122,18 +128,54 @@ class Cannon {
 
   show(images) {
     this.image = images;
-    image(this.image, this.x, this. y, this.height, this.width);
+    image(this.image, this.x, this.y, this.height, this.width);
   }
 }
 
 class FiringBall extends Cannon {
   constructor() {
-    
+    super();
+    this.x = 0;
+    this.y = 0;
+    this.speedY = -10;
+    this.inertia = 0;
+    this.fired = false;
   }
 
-  super.show(blueBubble);
-    //console.log(this.inertia);
-  //}
+  initialize(x, y, colour) {
+    this.x = x;
+    this.y = y;
+    this.colour = colour;
+    this.inertia = this.direction/5
+    this.fired = true;
+    this.kill = false;
+  }
 
+  move() {
+    console.log("Operate");
+    if (this.fired) {
+      this.y += this.speedY;
+      this.x += this.inertia;
+    }
+  }
 
+  display() {
+    if (!this.kill) {
+      image(this.colour, this.x, this.y, GRID_SIZE, GRID_SIZE);
+    }
+  }
+
+  checkCollision() {
+    for (let row of grid) {
+      for (let cell of row) {
+        if (cell.active && dist(this.x, this.y, cell.xPosition, cell.yPosition) < GRID_SIZE) {
+          this.fired = false;
+          cell.active = false; 
+          bubbleFired = false; 
+          this.kill = true;
+          return;
+        }
+      }
+    }
+  }
 }
